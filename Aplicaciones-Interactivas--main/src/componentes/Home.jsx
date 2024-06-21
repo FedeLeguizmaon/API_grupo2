0cbff6e317421f70c47ac05dc84fb61876a97ca0
nicosicalo <nicolassicalo@gmail.com>
2024-06-21 18:43:37 -0300

    imagenes

--- Aplicaciones-Interactivas--main/src/componentes/Home.jsx
+++ Aplicaciones-Interactivas--main/src/componentes/Home.jsx
@@ -1,32 +1,32 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Catalogo from './Catalogo';
 import Carrito from './Carrito';
 import Navbar from './Navbar';
 import "./estilos/HomeStyles.css"
 
-
-import { useNavigate } from 'react-router-dom';
-
 const Home = () => {
-  
   const navigate = useNavigate();
-  const[mostarCatalogo,setMostrarCatalogo] = useState(true);
+  const [mostrarCatalogo, setMostrarCatalogo] = useState(true);
+
   const handleVerCarrito = () => {
-    navigate('/Carrito')
+    navigate('/Catalogo');
   };
 
   return (
     <main>
-      
       <header>
-      
         <nav>
-          <ul>
-            <li><button onClick={handleVerCarrito}>!Comenzar a Comprar!</button></li>
-            
-          </ul>
         </nav>
+        <h1>Bienvenido a Remeras Originals</h1>
+        <div className="tarjeta-mensaje" onClick={handleVerCarrito}>
+          <h1>¡Clickea aca para Comprar tus mejores remeras en esta página!</h1>
+        </div>
       </header>
+      
+      <footer>
+        <p>© 2024 Tienda de Remeras. Todos los derechos reservados.</p>
+      </footer>
     </main>
   );
 };
